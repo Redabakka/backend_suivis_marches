cec171ea96adff926ac7c1e7e6620557e6335e57
bahaeddiine12 <bahaeddiine12@gmail.com>
2025-11-10 19:07:22 +0100

    Add a comment in Main class

--- src/main/java/uir/ac/ma/suivi_marches/SuiviMarchesApplication.java
+++ src/main/java/uir/ac/ma/suivi_marches/SuiviMarchesApplication.java
@@ -11,3 +11,7 @@ public class SuiviMarchesApplication {
     }
 
 }
+
+// IMPORTANT: There is a documentation of every endpoint with swagger.
+// Use the URL to access it after running the app: http://localhost:8080/swagger-ui/index.html
+// Use the port that you're using, in my case is 8080 (as in application.properties)
